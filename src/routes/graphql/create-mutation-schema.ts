90e9c17375ea6c5b578150a1678d1dfe054fea66
Lotkovsky <dmitry.lotkov9@gmail.com>
2025-05-25 16:48:32 +0300

    feat: add delete entity mutations

--- src/routes/graphql/create-mutation-schema.ts
+++ src/routes/graphql/create-mutation-schema.ts
@@ -1,4 +1,4 @@
-import { GraphQLObjectType, GraphQLNonNull } from 'graphql/index.js';
+import { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLError } from 'graphql/index.js';
 import { PostType, UserType, ProfileType } from './types/query-types.js';
 import {
   CreatePostInputType,
@@ -7,6 +7,7 @@ import {
   Context,
   PostBody, UserBody, ProfileBody,
 } from './types/mutation-types.js';
+import { UUIDType } from './types/uuid.js';
 
 export const createMutationSchema:() => GraphQLObjectType<Record<string, never>, Context> = () => {
   return new GraphQLObjectType<Record<string, never>, Context>({
@@ -56,6 +57,69 @@ export const createMutationSchema:() => GraphQLObjectType<Record<string, never>,
               memberTypeId: args.dto.memberTypeId
             }
           })
+        }
+      },
+      deletePost: {
+        type: new GraphQLNonNull(GraphQLString),
+        args: {
+          id: { type: new GraphQLNonNull(UUIDType) },
+        },
+        resolve: async(_src, args: { id: string}, context: Context) => {
+          const isExisting = await context.prisma.post.findUnique({
+            where: { id: args.id },
+          });
+
+          if (!isExisting) {
+            throw new GraphQLError(`Post with id ${args.id} not found`);
+          }
+
+          await context.prisma.post.delete({
+            where: { id: args.id },
+          });
+
+          return `Post ${args.id} deleted`;
+        }
+      },
+      deleteProfile: {
+        type: new GraphQLNonNull(GraphQLString),
+        args: {
+          id: { type: new GraphQLNonNull(UUIDType) },
+        },
+        resolve: async (_src, args: { id: string}, context: Context) => {
+            const isExisting = await context.prisma.profile.findUnique({
+              where: { id: args.id }
+            })
+
+            if (!isExisting) {
+              throw new GraphQLError(`Profile with id ${args.id} not found`);
+            }
+
+            await context.prisma.profile.delete({
+              where: { id: args.id }
+            })
+
+          return `Profile ${args.id} deleted`
+        }
+      },
+      deleteUser: {
+        type: new GraphQLNonNull(GraphQLString),
+        args: {
+          id: { type: new GraphQLNonNull(UUIDType) },
+        },
+        resolve: async (_src, args: { id: string}, context: Context) =>{
+          const isExisting = await context.prisma.user.findUnique({
+            where: { id: args.id },
+          })
+
+          if (!isExisting) {
+            throw new GraphQLError(`User with id ${args.id} not found`);
+          }
+
+          await context.prisma.user.delete({
+            where: { id: args.id }
+          })
+
+          return `User ${args.id} deleted`
         }
       }
     }
--- src/routes/graphql/types/query-types.ts
+++ src/routes/graphql/types/query-types.ts
@@ -45,8 +45,8 @@ export const PostType = new GraphQLObjectType({
   name: 'Post',
   fields: {
     id: { type: new GraphQLNonNull(UUIDType) },
-    title: { type: new GraphQLNonNull(UUIDType) },
-    content: { type: new GraphQLNonNull(UUIDType) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
   },
 });
 
